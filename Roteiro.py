c158a6a2be6707252dd8c62277b3e56974e69262
AyrtonDias62 <ayrton.bdias@gmail.com>
2026-03-22 22:13:18 -0300

    Update Roteiro.py

--- Roteiro.py
+++ Roteiro.py
@@ -52,7 +52,7 @@ def get_coords_cep(cep_raw, num_raw, _ors_key):
 # --- 2. SETUP ---
 ORS_KEY = st.secrets["ORS_KEY"]
 ors_client = client.Client(key=ORS_KEY)
-u_base = {"endereco": "Unidade Matriz SBC", "lat": -23.6911764, "lon": -46.561668}     # CEP 09750-601 - Av. Índico 900
+u_base = {"endereco": "Unidade Matriz SBC", "lat": -23.691297, "lon": -46.5590672}     # CEP 09750-601 - Av. Índico 900
                                                               # Na Lucas 907 -23.6908783   -46.5584661   09750-670
 # --- 3. SIDEBAR ---
 with st.sidebar:
